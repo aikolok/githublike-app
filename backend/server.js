f37bac87f8134f670c4f8965223f146324ef0a71
aikolok <skybloom5878@gmail.com>
2024-02-24 19:55:42 -0600

    updated server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
+import path from "path";
 
 import "./passport/github.auth.js";
 
@@ -15,6 +16,8 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+const __dirname = path.resolve();
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -25,15 +28,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(4000, () => {
-  console.log("Server started on http://localhost:4000");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
   connectMongoDB();
 });
